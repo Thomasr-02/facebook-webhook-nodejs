771bfeab56fb2444750d739aa84754dcc3078010
Thomas Ribeiro <thomas10191@hotmail.com>
2021-04-08 14:20:52 -0300

    Added a Procfile.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post( '/webhook' , ( req , res ) => {
   // Checks this is an event from a page subscription 
   if ( body.object==='page') {
       // Iterates over each entry - there may be multiple if batched 
-      console.log("Here:", body.entry);
+      console.log("Here:", body.entry.changes);
       body.entry.forEach(function(entry,index){ 
 
         // Gets the message. entry.messaging is an array, but // will only ever contain one message, so we get index 0 
